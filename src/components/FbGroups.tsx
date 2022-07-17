6e31d4c09390dab816fadcb7460fd6d5f2ec709c
Huynh Duc Dung <dunghd.it@gmail.com>
2022-07-17 23:42:06 +0800

    :sparkles: add to fav on group/page

--- src/components/FbGroups.tsx
+++ src/components/FbGroups.tsx
@@ -2,11 +2,12 @@ import { useAtom } from "jotai";
 import React from "react";
 import { useInfiniteQuery } from "react-query";
 
-import { selectedFacebookIdsAtom } from "../store";
+import { favoriteFacebookIdsAtom, selectedFacebookIdsAtom } from "../store";
 import { getFacebookGroup } from "../utils/api";
 
 export const FbGroups = ({ accessToken }: { accessToken: string }) => {
   const [ids, setIds] = useAtom(selectedFacebookIdsAtom);
+  const [favoriteIds, setFavoriteIds] = useAtom(favoriteFacebookIdsAtom);
 
   const {
     isLoading,
@@ -48,8 +49,38 @@ export const FbGroups = ({ accessToken }: { accessToken: string }) => {
           {currentGroup.groups.map((group) => (
             <div
               key={group.id}
-              className="mx-auto card card-side w-full h-44 bg-base-100 shadow-xl"
+              className={
+                favoriteIds.includes(group.id)
+                  ? "mx-auto card card-side w-full h-44 bg-yellow-200 shadow-xl"
+                  : "mx-auto card card-side w-full h-44 bg-base-100 shadow-xl"
+              }
             >
+              <div>
+                <button
+                  onClick={() =>
+                    setFavoriteIds(
+                      favoriteIds.includes(group.id)
+                        ? favoriteIds.filter((id) => id !== group.id)
+                        : [...favoriteIds, group.id]
+                    )
+                  }
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="h-6 w-6"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                    strokeWidth={2}
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M11.049 2.927c.3-.921 1.603-.921 1.902 0l1.519 4.674a1 1 0 00.95.69h4.915c.969 0 1.371 1.24.588 1.81l-3.976 2.888a1 1 0 00-.363 1.118l1.518 4.674c.3.922-.755 1.688-1.538 1.118l-3.976-2.888a1 1 0 00-1.176 0l-3.976 2.888c-.783.57-1.838-.197-1.538-1.118l1.518-4.674a1 1 0 00-.363-1.118l-3.976-2.888c-.784-.57-.38-1.81.588-1.81h4.914a1 1 0 00.951-.69l1.519-4.674z"
+                    />
+                  </svg>
+                </button>
+              </div>
               {group.picture?.data?.url && (
                 <figure>
                   <img src={group.picture?.data?.url} alt={group.name} />
@@ -63,6 +94,7 @@ export const FbGroups = ({ accessToken }: { accessToken: string }) => {
                 >
                   <h2 className="card-title">{group.name}</h2>
                 </a>
+
                 <p className="text-clip overflow-hidden">{group.description}</p>
                 <div className="card-actions justify-end">
                   <button
--- src/store.ts
+++ src/store.ts
@@ -1,3 +1,4 @@
 import { atom } from "jotai";
 
 export const selectedFacebookIdsAtom = atom<number[]>([]);
+export const favoriteFacebookIdsAtom = atom<number[]>([]);
